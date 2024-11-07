6d62d4ce064d9b2d5e6268f260d46143089590d8
wooahan <all_11@naver.com>
2024-11-08 04:42:57 +0900

    main

--- next.config.js
+++ next.config.js
@@ -5,21 +5,16 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
   eslint: {
-    ignoreDuringBuilds: true, // 빌드 시 ESLint 경고 무시
+    ignoreDuringBuilds: true,
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
-
-    // 클라이언트에서 fs, net, http2 사용 방지
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        http2: false,
-      };
-    }
-
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      http2: false,
+    };
     return config;
   },
 };
--- pages/admin/match-entry.tsx
+++ pages/admin/match-entry.tsx
@@ -1,15 +1,10 @@
 // File: pages/admin/match-entry.tsx
 import dynamic from 'next/dynamic';
 
-// SSR을 비활성화한 상태로 MatchEntry 컴포넌트를 동적으로 가져오기
 const MatchEntry = dynamic(() => import('../../components/MatchEntry'), { ssr: false });
 
 const MatchEntryPage = () => {
-  return (
-    <div>
-      <MatchEntry />
-    </div>
-  );
+  return <MatchEntry />;
 };
 
 export default MatchEntryPage;
