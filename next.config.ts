8466b740f8e1e6a322144c71cfcb3f4d149292b5
wooahan <all_11@naver.com>
2024-11-07 16:27:52 +0900

    main

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // 빌드 시 ESLint 무시
   },
+  trailingSlash: true, // 모든 경로 뒤에 슬래시를 추가
+  reactStrictMode: true, // React의 Strict Mode 활성화
+  swcMinify: true, // 빌드 속도를 향상시키기 위한 swc 사용
+  output: 'standalone', // Vercel의 Docker 배포 호환성 확보를 위해
   /* 다른 설정 옵션이 있다면 여기에 추가하세요 */
 };
 
