368bd3a2ee652237c86f5c3105ba7f4e7d93ba9f
wooahan <all_11@naver.com>
2024-11-07 18:31:43 +0900

    main

--- pages/admin/player-management.tsx
+++ pages/admin/player-management.tsx
@@ -106,6 +106,21 @@ const PlayerManagement = () => {
             <option value="올라운더">올라운더</option>
           </select>
         </div>
+        <div>
+          <label>티어</label>
+          <select {...register('tier', { required: true })}>
+            <option value="">티어 선택</option>
+            <option value="아이언">아이언</option>
+            <option value="브론즈">브론즈</option>
+            <option value="실버">실버</option>
+            <option value="골드">골드</option>
+            <option value="플레티넘">플레티넘</option>
+            <option value="에메랄드">에메랄드</option>
+            <option value="다이아몬드">다이아몬드</option>
+            <option value="마스터">마스터</option>
+            <option value="그랜드마스터">그랜드마스터</option>
+          </select>
+        </div>
         <button type="submit">선수 추가/수정</button>
       </form>
       <div>
@@ -118,6 +133,7 @@ const PlayerManagement = () => {
                 <p>닉네임: {player.nickname}</p>
                 <p>메인 포지션: {player.mainPosition}</p>
                 <p>서브 포지션: {player.secondaryPosition}</p>
+                <p>티어: {player.tier}</p>
                 <button onClick={() => reset(player)}>수정</button>
                 <button onClick={() => deletePlayer(player.id)}>삭제</button>
               </li>
