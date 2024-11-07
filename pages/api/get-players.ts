4e5c5c14a35fb5624f67a2ded2cd3d75b00992ad
wooahan <all_11@naver.com>
2024-11-08 03:24:01 +0900

    main

--- pages/api/get-players.ts
+++ pages/api/get-players.ts
@@ -3,13 +3,18 @@ import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-  });
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\n/g, '\n'),
+      }),
+    });
+    console.log('Firebase Admin Initialized Successfully');
+  } catch (error) {
+    console.error('Error initializing Firebase Admin:', error);
+  }
 }
 
 const db = getFirestore();
@@ -17,10 +22,13 @@ const db = getFirestore();
 const handler = async (req, res) => {
   if (req.method === 'GET') {
     try {
+      console.log('Fetching players from Firestore...');
       const querySnapshot = await db.collection('선수 정보').get();
       const playerList = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      console.log('Players fetched:', playerList);
       res.status(200).json(playerList);
     } catch (error) {
+      console.error('Error fetching players from Firestore:', error);
       res.status(500).json({ error: 'Failed to fetch players' });
     }
   } else {
