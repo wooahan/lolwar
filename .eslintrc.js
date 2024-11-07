167c43548a270e433c65a435515e315b91fff170
wooahan <all_11@naver.com>
2024-11-07 15:58:16 +0900

    main

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "next/typescript"], // 기존 설정 확장
+  extends: ["next/core-web-vitals", "next/typescript"], // Next.js 기본 설정 확장
   rules: {
     '@typescript-eslint/no-unused-vars': 'off', // 사용되지 않는 변수 오류 무시
     '@typescript-eslint/no-explicit-any': 'off', // any 사용 경고 무시
