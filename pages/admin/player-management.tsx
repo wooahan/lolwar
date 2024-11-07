cc5a23e9e0dc3361fc25bebe434d6db8018cec24
wooahan <all_11@naver.com>
2024-11-07 18:14:30 +0900

    main

--- pages/admin/player-management.tsx
+++ pages/admin/player-management.tsx
@@ -113,25 +113,3 @@ const PlayerManagement = () => {
 };
 
 export default PlayerManagement;
-
-// File: pages/index.tsx (Main Page)
-import Link from 'next/link';
-
-const HomePage = () => {
-  return (
-    <div>
-      <h1>롤 내전 전적 관리 서비스</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/admin/player-management">
-              <a>선수 관리</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
-
-export default HomePage;
