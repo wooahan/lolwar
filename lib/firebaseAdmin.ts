cc0b8af7af3e09a259e648bfdcbc5c9db2a18517
wooahan <all_11@naver.com>
2024-11-07 23:30:33 +0900

    main

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,15 +1,10 @@
-// File: lib/firebaseAdmin.ts
-
 import admin from 'firebase-admin';
-import path from 'path';
-
-// 비공개 키 파일 경로를 require를 사용하여 가져오기
-const serviceAccount = require(path.resolve(__dirname, '../keys/serviceAccountKey.json')); // 실제 비공개 키 파일 경로로 수정하세요.
+// require를 사용하여 JSON 파일을 가져옴 (경로 확인 필수)
+const serviceAccount = require('../keys/serviceAccountKey.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://lolwar-6080e.firebaseio.com" // 실제 Firebase 데이터베이스 URL로 수정하세요.
   });
 }
 
