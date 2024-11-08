c1fdd94d3683db06d0c159d84a443028ad56dc9a
wooahan <all_11@naver.com>
2024-11-08 21:50:17 +0900

    main

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-// next.config.js
-
-const path = require('path'); // 추가
-
+const path = require('path');
 const withTM = require('next-transpile-modules')([
   'react-dnd',
   'dnd-core',
@@ -12,29 +9,17 @@ const withTM = require('next-transpile-modules')([
 
 const nextConfig = withTM({
   reactStrictMode: true,
-  trailingSlash: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'react/jsx-runtime.js': require.resolve('react/jsx-runtime'),
-      'react/jsx-dev-runtime.js': require.resolve('react/jsx-dev-runtime'),
-    };
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
       net: false,
       http2: false,
     };
-
-    // 서버와 클라이언트 각각에 대해 다른 설정을 추가할 수 있음
-    if (!isServer) {
-      config.resolve.alias['@components'] = path.resolve(__dirname, 'components');
-    }
-
     return config;
   },
 });
